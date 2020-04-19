1d7f19842d659ae3c0546e4e0e84232e7c73a37f
SayChunKim <saychunkim@gmail.com>
2020-04-19 22:55:08 +0800

    Minor Fix for Formspree Redirect

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.af045a766609f939323e3bdd17c97822.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.0cb06a1bcfb0c30c4b9b5eb16e972e96.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
