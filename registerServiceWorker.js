c6eb9888d8c30087d2395f2a10d5dde0518d3a54
SayChunKim <saychunkim@gmail.com>
2020-04-23 17:45:58 +0800

    Update Styling and Projects Section

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.af045a766609f939323e3bdd17c97822.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.9568997c759b952b8cd8f143cf56a327.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
