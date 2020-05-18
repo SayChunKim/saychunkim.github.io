49690b0c8a75ea707e076ba456f60646256eb211
SayChunKim <saychunkim@gmail.com>
2020-05-19 01:23:00 +0800

    Web Perf and Asset Fixes

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.6b954d5d0103b9f7dbe16ebd217b0a6d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.b3375d811522024e04a5db2ae100f14f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
