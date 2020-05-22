1aa6b4e85d8782e4758800dbff6962b587d646ce
SayChunKim <saychunkim@gmail.com>
2020-05-22 16:26:34 +0800

    Updated SW and ImageMin

---
+++ precache-manifest.333e5a8a600583e1804e403904906256.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379b68a096cbffc7fa65",
+    "url": "/css/app.fd8bfad7.css"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/fonts/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/fonts/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/fonts/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/fonts/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/fonts/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/fonts/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/fonts/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/fonts/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/fonts/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/fonts/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/fonts/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/fonts/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "a5a8a66847a0fa86485fc5530a1fa6a9",
+    "url": "/img/fa-brands-400.a5a8a668.svg"
+  },
+  {
+    "revision": "88f0c5bd0da36b4a5d3cadbb793254cd",
+    "url": "/img/fa-regular-400.88f0c5bd.svg"
+  },
+  {
+    "revision": "6c36adff85a9daa51869d2ce16609c9f",
+    "url": "/img/fa-solid-900.6c36adff.svg"
+  },
+  {
+    "revision": "62c056399f8dceedc38711b668c7e04b",
+    "url": "/img/timeline/cover_spa.JPG"
+  },
+  {
+    "revision": "fa15d13ee51f21ab2edb28396ed86d8b",
+    "url": "/img/timeline/fusionex_1.jpg"
+  },
+  {
+    "revision": "3cea277a67586c43f14bad0514323b9d",
+    "url": "/img/timeline/gdg_2.jpg"
+  },
+  {
+    "revision": "106e8f5fb128683d6f5a0f4b861dd103",
+    "url": "/img/timeline/gdg_4.jpg"
+  },
+  {
+    "revision": "e90ea22384bfd9dab8445260b3cde55e",
+    "url": "/img/timeline/gsa_3.jpg"
+  },
+  {
+    "revision": "2ad5a7eeb3ac1f4188ed40377aae6e45",
+    "url": "/img/timeline/havenice_6.jpg"
+  },
+  {
+    "revision": "b33e7c38cd85ad3e79a54db60e38ede1",
+    "url": "/img/timeline/self_4.jpg"
+  },
+  {
+    "revision": "d82f21d181d4458b83800db28a75e750",
+    "url": "/img/timeline/xda_1.jpg"
+  },
+  {
+    "revision": "2f95cb379564185530ad2a79f0a88045",
+    "url": "/img/works/EstoreMHD.JPG"
+  },
+  {
+    "revision": "131b8e37dee2482eaf5ca4704b69c84d",
+    "url": "/img/works/Github_SCKim.jpg"
+  },
+  {
+    "revision": "8d94b59a648a14f6a33397b61bd538eb",
+    "url": "/img/works/coldstorage.jpg"
+  },
+  {
+    "revision": "6c3c4dc7b7e0f14c8a8b37a5022d11fc",
+    "url": "/img/works/ioxKL2016.jpg"
+  },
+  {
+    "revision": "c6e8ff78ec311b5f12944e50dc5bb802",
+    "url": "/img/works/mhd_estore.jpg"
+  },
+  {
+    "revision": "afa1fc1cf1861c69c8351fc30506e509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379b68a096cbffc7fa65",
+    "url": "/js/app.73794908.js"
+  },
+  {
+    "revision": "a0bf6835c292151ea92f",
+    "url": "/js/chunk-vendors.53f17768.js"
+  },
+  {
+    "revision": "6e976b8a5aee4710ed38e98eaaa8fb05",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "1c249bc91e52a3c1dbf9a2b08e8f92eb",
+    "url": "/pdfs/Devfest_KK _Polymer _ Packaging_Web_App_into_Mobile_Apps.pdf"
+  },
+  {
+    "revision": "da8b6e9d2ccd777d64488c55a5641ebe",
+    "url": "/pdfs/Firebase_ A New_Future_for_Tackling_Back-End_Authentication.pdf"
+  },
+  {
+    "revision": "7516ec53e1399d006cd70cde2621c302",
+    "url": "/pdfs/KLJS_Introduction_to_PWA.pdf"
+  },
+  {
+    "revision": "62a6ba04f363f85ac22acebcc7399763",
+    "url": "/pdfs/Payment_Request_API_MOSC.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/service-worker.js"
+  }
+]);
